cd2bbb56645f33e9481b678313c194f977bc4d34
Nahuel Agustin Dumo <nahueldumo2014@gmail.com>
2025-03-19 00:51:25 -0300

    Listo herencia

--- models/project_project.py
+++ models/project_project.py
@@ -7,8 +7,20 @@ class ProjectProject(models.Model):
         default = dict(default or {})
         new_project = super(ProjectProject, self).copy(default)
 
-        # Asegurar que las tareas del nuevo proyecto conserven su etapa
-        for task in new_project.task_ids:
-            task.write({'stage_id': task._origin.stage_id.id})
+        # Diccionario para mapear las tareas originales con sus copias
+        task_mapping = {}
+
+        # Primero, duplicamos las tareas principales y las guardamos en el diccionario
+        for task in self.task_ids.filtered(lambda t: not t.parent_id):
+            new_task = task.copy({'project_id': new_project.id})
+            task_mapping[task.id] = new_task.id
+
+        # Luego, duplicamos las subtareas y las vinculamos correctamente
+        for task in self.task_ids.filtered(lambda t: t.parent_id):
+            new_task = task.copy({
+                'project_id': new_project.id,
+                'parent_id': task_mapping.get(task.parent_id.id),  # Mapea la nueva tarea padre
+            })
+            task_mapping[task.id] = new_task.id
 
         return new_project
